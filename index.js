63906fc8e24fe74fc870d99c3cfba440a10b066c
Saadat Badgujar <saadatbadgujar@gmail.com>
2022-01-09 20:13:36 +0530

    Added basic layout using CSS and refactored index.html

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 // DECLARATIONS
 
 const submit = document.querySelector("#submit");
@@ -42,12 +41,12 @@ function display() {
     let myBooks = JSON.parse(localStorage.getItem("books"));
     for (let i = 0; i < myBooks.length; i++) {
       card = document.createElement("div");
+      card.classList.add("card");
       card.innerText = `${myBooks[i]["title"]} by ${myBooks[i]["author"]}, ${myBooks[i]["pages"]} pages, ${myBooks[i]["read"]}`;
       container.appendChild(card);
     }
   }
 }
-
 
 function clear() {
   // Clears the container in the DOM for Re-rendering the page with additional books if added
@@ -58,7 +57,6 @@ function reset() {
   // Resets the HTML Book Form after submission
   document.getElementById("myForm").reset();
 }
-
 
 // EVENT LISTENERS
 
